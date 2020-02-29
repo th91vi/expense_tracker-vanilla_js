a0c8c67636f944f82e8cd8b92b14f0d2ec052efd
Thiago Vieira <th91v@outlook.com>
2020-02-29 13:12:23 -0300

    correção de tag no HTML, adição e subtração de transações na interface

--- app.js
+++ app.js
@@ -17,6 +17,35 @@ const dummyTransactions = [ // transações mocadas temporárias
 
 let transactions = dummyTransactions;
 
+// adciona transação na aplicação
+function addTransaction(e) {
+    e.preventDefault();
+
+    if (text.value.trim() === '' || amount.value.trim() === '') {
+        alert("Please insert a title and amount for the expense")
+    } else {
+        const transaction = {
+            id: generateID(),
+            text: text.value,
+            amount: +amount.value // sinal de + foi inserido para trabsformar o valor em um Number, senão receberiamos um erro 'Uncaught TypeError'
+        }
+        
+        transactions.push(transaction);
+
+        addTransactionDOM(transaction);
+
+        updateValues();
+
+        text.value = '';
+        amount.value = '';
+    }
+}
+
+// gera ID aleatória
+function generateID(){
+    return Math.floor(Math.random() * 1000000000); // susbtituir esta lógica por um RANGE
+}
+
 // adiciona transações na lista do DOM
 function addTransactionDOM(transaction){
     // pega sinal da transação
@@ -30,7 +59,7 @@ function addTransactionDOM(transaction){
     item.classList.add(transaction.amount < 0 ? 'minus' : 'plus');
 
     item.innerHTML = `
-        ${transaction.text} <span>${sign}${Math.abs(transaction.amount)}</span> <button class="delete-btn">x</button>
+        ${transaction.text} <span>${sign}${Math.abs(transaction.amount)}</span> <button class="delete-btn" onclick="removeTransaction(${transaction.id})">x</button>
     `;
 
     list.appendChild(item);
@@ -40,7 +69,8 @@ function addTransactionDOM(transaction){
 function updateValues(){
     const amounts = transactions.map(transaction => transaction.amount);
 
-    const total = amounts.reduce((acc, item) => (acc += item), 0).toFixed(2);
+    const total = amounts.reduce((acc, item) => (acc += item), 0)
+    .toFixed(2);
     
     const income = amounts // calcula total de receitas
     .filter(item => item > 0)
@@ -57,6 +87,13 @@ function updateValues(){
     money_minus.innerHTML = `$${expense}`;
 }
 
+// remove transação pela ID
+function removeTransaction(id) {
+    transactions = transactions.filter(transaction => transaction.id !== id);
+
+    init();
+}
+
 // ativa estado inicial do app
 function init(){
     list.innerHTML = '';
@@ -65,3 +102,5 @@ function init(){
 }
 
 init();
+
+form.addEventListener('submit', addTransaction);
